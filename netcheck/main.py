5b6058e0621f1488ee9db3f0a73e758f9b3add23
dannywade <danny.wade35@gmail.com>
2021-09-14 15:31:29 -0400

    added basic unit tests for frontend views

--- netcheck/main.py
+++ netcheck/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
+import os
 # Local imports
-from api.api_v1.api import api_router
+from netcheck.api.api_v1.api import api_router
+
+app_dir = os.path.dirname(__file__)
 
 api_description = "API used to validate and performs tests against your network"
 
@@ -15,8 +18,8 @@ app = FastAPI(description=api_description,
 app.include_router(api_router, prefix="/api/v1")
 
 # Link app to static files and templates
-app.mount("/frontend/static", StaticFiles(directory="./frontend/static"), name="static")
-templates = Jinja2Templates(directory="./frontend/templates")
+app.mount("/frontend/static", StaticFiles(directory=f"{app_dir}/frontend/static"), name="static")
+templates = Jinja2Templates(directory=f"{app_dir}/frontend/templates")
 
 # Frontend views
 @app.get("/", response_class=HTMLResponse)
--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -4,7 +4,18 @@ from netcheck.main import app
 # Initialize the test client
 client = TestClient(app)
 
-def test_read_main():
+def test_index_page():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+
+def test_about_page():
+    response = client.get("/about")
+    assert response.status_code == 200
+
+def test_validation_page():
+    response = client.get("/validation")
+    assert response.status_code == 200
+
+def test_analysis_page():
+    response = client.get("/analysis")
+    assert response.status_code == 200
